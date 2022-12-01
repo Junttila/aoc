b80b83b0d80e1af89cfcb89375a780b3e8441538
Joakim Junttila <joakim@juntti.la>
2022-12-01 07:01:51 +0100

    Enabling example usage

--- src/index.ts
+++ src/index.ts
@@ -7,48 +7,59 @@ import path from 'node:path';
 import fetch from 'cross-fetch';
 import { env } from 'process';
 
+const EXAMPLE = true;
 const YEAR = 2022;
 const DAY = 1;
 const AOC_INPUT_URI = `https://adventofcode.com/${YEAR}/day/${DAY}/input`;
-const INPUT_FILE_NAME = `src/solutions/day${DAY}/input.txt`;
+const INPUT_FILE_NAME = `src/solutions/day${DAY}/${
+  EXAMPLE ? 'example' : 'input'
+}.txt`;
 const AOC_SESSION = env.AOC_SESSION || '';
 let currentFile;
-try {
-  currentFile = fs.readFileSync(
-    path.resolve(filePathForJS(INPUT_FILE_NAME)),
-    'utf8',
-  );
-} catch (e) {
-  console.log("Couldn't read file, touching", INPUT_FILE_NAME);
-  fs.writeFile(INPUT_FILE_NAME, '', (a) => {
-    if (a !== null) {
-      throw a;
-    }
-  });
+if (!EXAMPLE) {
+  try {
+    currentFile = fs.readFileSync(
+      path.resolve(filePathForJS(INPUT_FILE_NAME)),
+      'utf8',
+    );
+  } catch (e) {
+    console.log("Couldn't read file, touching", INPUT_FILE_NAME);
+    fs.writeFile(INPUT_FILE_NAME, '', (a) => {
+      if (a !== null) {
+        throw a;
+      }
+    });
+  }
 }
 (async () => {
-  if (!currentFile) {
-    console.log('Input file empty!');
-    console.log('Fetching data for year', YEAR, 'day', DAY);
-    const result = await fetch(AOC_INPUT_URI, {
-      headers: {
-        cookie: `session=${AOC_SESSION}`,
-      },
-    });
-    if (result.status === 200) {
-      fs.writeFile(INPUT_FILE_NAME, await result.text(), (a) => {
-        if (a !== null) {
-          throw a;
-        }
+  if (!EXAMPLE) {
+    if (!currentFile) {
+      console.log('Input file empty!');
+      console.log('Fetching data for year', YEAR, 'day', DAY);
+      const result = await fetch(AOC_INPUT_URI, {
+        headers: {
+          cookie: `session=${AOC_SESSION}`,
+        },
       });
-    }
-    if (result.status === 404) {
-      console.log(`404 returned, day ${DAY} not started?`);
+      if (result.status === 200) {
+        fs.writeFile(INPUT_FILE_NAME, await result.text(), (a) => {
+          if (a !== null) {
+            throw a;
+          }
+        });
+      }
+      if (result.status === 404) {
+        console.log(`404 returned, day ${DAY} not started?`);
+      }
     }
   }
 
   const inputAsLines = fileToArray(INPUT_FILE_NAME);
   console.log('Start day', DAY);
+  if (inputAsLines[0].length < 1) {
+    console.log('File empty, exiting');
+    return;
+  }
 
   solutions[DAY - 1].default.forEach((f, i) => {
     const before = new Date();
