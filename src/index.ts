bbce2badbd50ed31d256525cb85736c0fe133243
Joakim Junttila <joakim@juntti.la>
2022-12-04 10:47:02 +0100

    Day 4 solved

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import fetch from 'cross-fetch';
 import { env } from 'process';
 
 const YEAR = 2022;
-const DAY = 3;
+const DAY = 4;
 const AOC_INPUT_URI = `https://adventofcode.com/${YEAR}/day/${DAY}/input`;
 const EXAMPLE_FILE_NAME = `src/solutions/day${DAY}/example.txt`;
 const INPUT_FILE_NAME = `src/solutions/day${DAY}/input.txt`;
--- src/solutions/day4/solutions.ts
+++ src/solutions/day4/solutions.ts
@@ -1,14 +1,42 @@
-const solutions: Array<(lines: string[]) => string> = [
+const solutions: Array<(lines: string[]) => string | number> = [
   // Solution part 1
   (lines: string[]) => {
-    lines.length++;
-    return 'no answer';
+    return lines
+      .map((v) => {
+        const [a, b] = v.split(',');
+        return Number(
+          contains(
+            a.split('-').map((u) => Number.parseInt(u)),
+            b.split('-').map((u) => Number.parseInt(u)),
+          ),
+        );
+      })
+      .reduce((a, v2) => a + v2, 0);
   },
   // Solution part 2
   (lines: string[]) => {
-    lines.length++;
-    return 'no answer';
+    return lines
+      .map((v) => {
+        const [a, b] = v.split(',');
+        return Number(
+          overlaps(
+            a.split('-').map((u) => Number.parseInt(u)),
+            b.split('-').map((u) => Number.parseInt(u)),
+          ),
+        );
+      })
+      .reduce((a, v2) => a + v2, 0);
   },
 ];
+
+function contains([a1, a2]: number[], [b1, b2]: number[]): boolean {
+  return (a1 <= b1 && a2 >= b2) || (b1 <= a1 && b2 >= a2);
+}
+
+function overlaps([a1, a2]: number[], [b1, b2]: number[]): boolean {
+  return (
+    (a1 <= b1 && a2 >= b1) || (b2 >= a2 && b1 <= a2) || (b1 <= a1 && b2 >= a1)
+  );
+}
 
 export default solutions;
