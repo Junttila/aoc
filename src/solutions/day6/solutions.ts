4b1205742de77102cc2c8d57b25d15396b7fd6bf
Joakim Junttila <joakim@juntti.la>
2022-12-06 07:03:31 +0100

    Remove infinite loop state

--- src/solutions/day6/solutions.ts
+++ src/solutions/day6/solutions.ts
@@ -13,6 +13,9 @@ function findStart(data: string, nrOfDiff: number = 4): number | null {
   let allDiff = false;
   let i = nrOfDiff - 1;
   while (!allDiff) {
+    if (i >= data.length) {
+      return null;
+    }
     const sub = data.slice(i - nrOfDiff + 1, i + 1);
     const set = new Set<string>(sub.split(''));
     if (Array.from(set).length >= nrOfDiff) {
