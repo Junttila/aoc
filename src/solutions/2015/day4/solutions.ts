e0632d40bf7aaa1e7744f00a7e5c92bc2868dee8
Joakim Junttila <joakim@juntti.la>
2023-08-05 20:39:28 +0200

    2015 day 4 solved

--- src/solutions/2015/day4/solutions.ts
+++ src/solutions/2015/day4/solutions.ts
@@ -1,15 +1,32 @@
 import {Solution} from '../../../types';
+import * as md5 from 'md5';
 
 const solutions: Array<Solution> = [
   // Solution part 1
   (lines: string[]) => {
-    lines.length++;
-    return 'no answer';
+    const zeroes = 5;
+    const zeroesString = ''.padStart(zeroes, '0');
+    let i = 1;
+    for (; i < Number.MAX_SAFE_INTEGER; i++) {
+      const hash = md5(`${lines[0]}${i}`);
+      if (hash.slice(0, zeroes) === zeroesString) {
+        break;
+      }
+    }
+    return `${i}`;
   },
   // Solution part 2
   (lines: string[]) => {
-    lines.length++;
-    return 'no answer';
+    const zeroes = 6;
+    const zeroesString = ''.padStart(zeroes, '0');
+    let i = 1;
+    for (; i < Number.MAX_SAFE_INTEGER; i++) {
+      const hash = md5(`${lines[0]}${i}`);
+      if (hash.slice(0, zeroes) === zeroesString) {
+        break;
+      }
+    }
+    return `${i}`;
   },
 ];
 
