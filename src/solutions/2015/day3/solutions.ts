9ba04f9971f75bc0faf25dd666c7dfd7fa3f816c
Joakim Junttila <joakim@juntti.la>
2023-08-05 20:08:53 +0200

    2015 day 4 solved

--- src/solutions/2015/day3/solutions.ts
+++ src/solutions/2015/day3/solutions.ts
@@ -3,14 +3,68 @@ import {Solution} from '../../../types';
 const solutions: Array<Solution> = [
   // Solution part 1
   (lines: string[]) => {
-    lines.length++;
-    return 'no answer';
+    const instr = lines[0];
+    const visited = new Set<string>(['0,0']);
+    let pos = {x: 0, y: 0};
+
+    (instr.split('') as Array<Dir>).forEach(v => {
+      pos = move(pos, v);
+      visited.add(`${pos.x},${pos.y}`);
+    });
+
+    return visited.size;
   },
   // Solution part 2
   (lines: string[]) => {
-    lines.length++;
-    return 'no answer';
+    const instr = lines[0];
+    const visited = new Set<string>(['0,0']);
+    const poss = [
+      {x: 0, y: 0},
+      {x: 0, y: 0},
+    ];
+
+    (instr.split('') as Array<Dir>).forEach((v, i) => {
+      const si = i % 2;
+
+      poss[si] = move(poss[si], v);
+      visited.add(`${poss[si].x},${poss[si].y}`);
+    });
+
+    return visited.size;
   },
 ];
+
+function move(pos: Pos, dirC: Dir) {
+  const dir = dirs[dirC];
+  pos.x += dir.x;
+  pos.y += dir.y;
+  return pos;
+}
+
+interface Pos {
+  x: number;
+  y: number;
+}
+
+type Dir = '^' | 'v' | '<' | '>';
+
+const dirs = {
+  '^': {
+    x: 0,
+    y: -1,
+  },
+  v: {
+    x: 0,
+    y: 1,
+  },
+  '<': {
+    x: -1,
+    y: 0,
+  },
+  '>': {
+    x: 1,
+    y: 0,
+  },
+};
 
 export default solutions;
