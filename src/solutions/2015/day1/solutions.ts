bed338002a50f8d09086da63b18895ae40fd6ebb
Joakim Junttila <joakim@juntti.la>
2023-08-05 18:12:48 +0200

    2015 day 1 solved

--- src/solutions/2015/day1/solutions.ts
+++ src/solutions/2015/day1/solutions.ts
@@ -3,13 +3,22 @@ import {Solution} from '../../../types';
 const solutions: Array<Solution> = [
   // Solution part 1
   (lines: string[]) => {
-    lines.length++;
-    return 'no answer';
+    return lines[0].split('').reduce((a, c) => {
+      return a + (c === '(' ? 1 : -1);
+    }, 0);
   },
   // Solution part 2
   (lines: string[]) => {
-    lines.length++;
-    return 'no answer';
+    const dirs = lines[0];
+
+    let floor = 0;
+    let step = 0;
+
+    while (floor > -1) {
+      floor += dirs[step] === '(' ? 1 : -1;
+      step++;
+    }
+    return step;
   },
 ];
 
