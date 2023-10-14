b8292065ae8dfe5dc0c17373c68f79d0b4ebf928
Joakim Junttila <joakim@juntti.la>
2023-10-14 19:03:50 +0200

    2022 day 12 solved

--- src/solutions/2022/day12/solutions.ts
+++ src/solutions/2022/day12/solutions.ts
@@ -4,12 +4,11 @@ const solutions: Array<Solution> = [
   // Solution part 1
   (lines: string[]) => {
     const map = lines.map(l => l.split(''));
-    // const distances = new Set<number>();
 
     const start = {x: 0, y: 0};
 
-    for (let x = 0, y = 0; y < map.length; y++) {
-      for (; x < map[0].length; x++) {
+    for (let y = 0; y < map.length; y++) {
+      for (let x = 0; x < map[0].length; x++) {
         if (map[y][x] === 'S') {
           start.x = x;
           start.y = y;
@@ -18,22 +17,28 @@ const solutions: Array<Solution> = [
       }
     }
 
-    // travel(start, map, distances);
-
-    // console.log(distances);
-
-    // const shortest = Array.from(distances.values())
-    //   .sort((a, b) => b - a)
-    //   .at(-1);
-    // return shortest || -1;
     const result = bfs(start, map);
-    // console.log(result?.path);
-    return result?.res;
+    return result.length;
   },
   // Solution part 2
   (lines: string[]) => {
-    lines.length++;
-    return 'no answer';
+    const map = lines.map(l => l.split(''));
+
+    const starts: Position[] = [];
+
+    for (let y = 0; y < map.length; y++) {
+      for (let x = 0; x < map[0].length; x++) {
+        if (map[y][x] === 'S' || map[y][x] === 'a') {
+          starts.push({x, y});
+        }
+      }
+    }
+
+    const result = starts.reduce(
+      (a, s) => Math.min(bfs(s, map).length, a),
+      Number.MAX_SAFE_INTEGER
+    );
+    return result;
   },
 ];
 
@@ -51,8 +56,8 @@ function bfs(start: Position, map: string[][]) {
       visited.add(`${x},${y}`);
 
       if (map[y][x] === 'E') {
-        console.log(path.reduce((a, p) => `${a}\n${p.x},${p.y}`, ''));
-        return {res: path.length - 1, path};
+        // console.log(_paintPath(map, path));
+        return {length: path.length - 1, path};
       }
 
       const dests: Position[] = [
@@ -75,65 +80,16 @@ function bfs(start: Position, map: string[][]) {
       });
     }
   }
+
+  return {length: Number.MAX_SAFE_INTEGER, path: []};
 }
 
-function travel(
-  current: Position,
-  map: string[][],
-  distances: Set<number>,
-  distance = 0,
-  visited = new Set<string>(),
-  log: Position[] = [],
-  shortest = new Map<string, number>()
-) {
-  // console.log(distance);
-  const {x, y} = current;
-  // console.log(visited);
-  const visitedString = `${x},${y}`;
-  log.push(current);
-  if (distance >= (shortest.get(visitedString) || Number.MAX_SAFE_INTEGER)) {
-    return;
-  }
-  const newVisited = new Set(visited);
-  newVisited.add(visitedString);
-  shortest.set(visitedString, distance);
-  // console.log(visitedString);
-  if (map[y][x] === 'E') {
-    distances.add(distance);
-    // console.log(log.reduce((a, l) => `${a}\n${l.x},${l.y}`, ''));
-    console.log(distance - log.length);
-    return;
-  }
-
-  if (visitedString === '28,25') {
-    const _a = 1 + 1;
-  }
-
-  const dests: Position[] = [
-    {x: x + 1, y},
-    {x: x - 1, y},
-    {x, y: y + 1},
-    {x, y: y - 1},
-  ].filter(
-    d => d.x < map[0].length && d.x >= 0 && d.y < map.length && d.y >= 0
-  );
-  // .sort((a, b) => height(map[b.y][b.x]) - height(map[a.y][a.x]));
-  dests.forEach(d => {
-    if (
-      !newVisited.has(`${d.x},${d.y}`) &&
-      height(map[d.y][d.x]) <= height(map[y][x]) + 1
-    ) {
-      travel(
-        {x: d.x, y: d.y},
-        map,
-        distances,
-        distance + 1,
-        newVisited,
-        Array.from(log),
-        shortest
-      );
-    }
+function _paintPath(map: string[][], path: Position[]) {
+  const canvas = Array.from(map);
+  path.forEach(p => {
+    canvas[p.y][p.x] = canvas[p.y][p.x].toUpperCase();
   });
+  return canvas.reduce((a, c) => `${a}\n${c.join('')}`, '');
 }
 
 function height(c: string) {
