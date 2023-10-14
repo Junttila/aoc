0dcb45eb928da2270b1d193e2bd12b58de9bb1b9
Joakim Junttila <joakim@juntti.la>
2023-10-14 09:32:38 +0200

    2015 day 8 solved

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import fetch from 'cross-fetch';
 import {env} from 'process';
 
 const YEAR = '2015';
-const DAY = 7;
+const DAY = 8;
 const AOC_INPUT_URI = `https://adventofcode.com/${YEAR}/day/${DAY}/input`;
 const EXAMPLE_FILE_NAME = `src/solutions/${YEAR}/day${DAY}/example.txt`;
 const INPUT_FILE_NAME = `src/solutions/${YEAR}/day${DAY}/input.txt`;
--- src/solutions/2015/day8/solutions.ts
+++ src/solutions/2015/day8/solutions.ts
@@ -3,13 +3,38 @@ import {Solution} from '../../../types';
 const solutions: Array<Solution> = [
   // Solution part 1
   (lines: string[]) => {
-    lines.length++;
-    return 'no answer';
+    const {code, mem} = lines.reduce(
+      (a, l) => {
+        let mem = 0;
+        for (let i = 1; i < l.length - 1; i++) {
+          mem++;
+          i += l.slice(i, i + 2) === '\\x' ? 3 : l[i] === '\\' ? 1 : 0;
+        }
+        return {mem: a.mem + mem, code: a.code + l.length};
+      },
+      {mem: 0, code: 0}
+    );
+    return code - mem;
   },
   // Solution part 2
   (lines: string[]) => {
-    lines.length++;
-    return 'no answer';
+    const {enc, code} = lines.reduce(
+      (a1, l) => {
+        const enc =
+          // Surrounding quotes
+          2 +
+          // Original length
+          l.length +
+          // Count of escaped characters
+          l
+            .split('')
+            .reduce((a2, c) => a2 + (c === '"' || c === '\\' ? 1 : 0), 0);
+
+        return {enc: a1.enc + enc, code: a1.code + l.length};
+      },
+      {enc: 0, code: 0}
+    );
+    return enc - code;
   },
 ];
 
