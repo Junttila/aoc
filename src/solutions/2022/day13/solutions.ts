9a798e3a407f94b9baa6e4a574e5bff08bd0050b
Joakim Junttila <joakim@juntti.la>
2023-10-15 16:39:18 +0200

    2022 day 13 part 1 solved with debug

--- src/solutions/2022/day13/solutions.ts
+++ src/solutions/2022/day13/solutions.ts
@@ -1,10 +1,20 @@
 import {Solution} from '../../../types';
+import {splitArray} from '../../scripts/splitArray';
 
 const solutions: Array<Solution> = [
   // Solution part 1
   (lines: string[]) => {
-    lines.length++;
-    return 'no answer';
+    const pairs = splitArray(lines, '');
+    // console.log(pairs[0][0]);
+    // console.log(toPacket(pairs[0][0]));
+    // console.log(sorted([9], [[8, 7, 6]]));
+    const depth = '';
+    return pairs.reduce((a, p, i) => {
+      const s = sorted(toPacket(p[0]), toPacket(p[1]), depth);
+      console.log(s, '\n');
+      // if (sorted(toPacket(p[0]), toPacket(p[1])) === null) console.log(p);
+      return a + (s ? i + 1 : 0);
+    }, 0);
   },
   // Solution part 2
   (lines: string[]) => {
@@ -12,5 +22,31 @@ const solutions: Array<Solution> = [
     return 'no answer';
   },
 ];
+
+type Packet = Array<Packet> | number;
+
+function sorted(l: Packet, r: Packet, depth: string): boolean | null {
+  depth = depth + ' ';
+  // console.log(depth, 'comparing\n', depth, l, '\n', depth, 'and\n', depth, r);
+  if (Number.isInteger(l) && Number.isInteger(r)) return l === r ? null : l < r;
+  if (Array.isArray(l) && !Array.isArray(r)) return sorted(l, [r], depth);
+  if (!Array.isArray(l) && Array.isArray(r)) return sorted([l], r, depth);
+
+  if (Array.isArray(r) && Array.isArray(l)) {
+    // if (l.length === 0 && r.length === 0) return true;
+    for (let i = 0; i < Math.max(l.length, r.length); i++) {
+      if (l.at(i) === undefined && r.at(i) !== undefined) return true;
+      if (r.at(i) === undefined && l.at(i) !== undefined) return false;
+      const s = sorted(l[i], r[i], depth);
+      if (s !== null) return s;
+    }
+  }
+  console.log('returning null', l, r);
+  return null;
+}
+
+function toPacket(p: string) {
+  return JSON.parse(p) as Packet;
+}
 
 export default solutions;
