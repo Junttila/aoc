8c2ecd18abdc59679e1a928d7dc0db1ff9fca5ab
Joakim Junttila <joakim@juntti.la>
2023-10-17 21:20:35 +0200

    2022 day 14 part 2 solved

--- src/solutions/2022/day14/solutions.ts
+++ src/solutions/2022/day14/solutions.ts
@@ -15,13 +15,25 @@ const solutions: Array<Solution> = [
       map[y][x] = 'o';
       sand = dropSand(map);
     }
-    console.log(_drawMap(map));
+
     return grains;
   },
   // Solution part 2
   (lines: string[]) => {
-    lines.length++;
-    return 'no answer';
+    const uniquePaths = Array.from(new Set(lines).values());
+    const map = createMap2(uniquePaths);
+    const start = {x: map[0].indexOf('+'), y: 0} as Position;
+
+    let sand = dropSand(map);
+    let grains = 1;
+    while (!(sand?.x === start.x && sand.y === start.y)) {
+      const {x, y} = sand!;
+      grains++;
+      map[y][x] = 'o';
+      sand = dropSand(map);
+    }
+
+    return grains;
   },
 ];
 
@@ -84,6 +96,60 @@ function createMap(input: string[]) {
       pos =>
         ({
           x: Number(pos.split(',')[0]) - minX,
+          y: Number(pos.split(',')[1]),
+        }) as Position
+    );
+
+    let s = start;
+    dests.forEach(d => {
+      let x = s.x,
+        y = s.y;
+      for (; Math.abs(x - d.x) !== 0 || Math.abs(y - d.y) !== 0; ) {
+        canvas[y][x] = '#';
+        x += x < d.x ? 1 : x > d.x ? -1 : 0;
+        y += y < d.y ? 1 : y > d.y ? -1 : 0;
+      }
+      canvas[y][x] = '#';
+      s = d;
+    });
+  });
+
+  return canvas;
+}
+
+function createMap2(input: string[]) {
+  const {maxY} = input
+    .map(l => l.split(' -> '))
+    .flat()
+    .reduce(
+      (a, p) => {
+        const [x, y] = p.split(',').map(Number);
+        return {
+          minX: x < a.minX ? x : a.minX,
+          maxX: x > a.maxX ? x : a.maxX,
+          maxY: y > a.maxY ? y : a.maxY,
+        };
+      },
+      {minX: 1000, maxX: 0, maxY: 0}
+    );
+
+  const canvas = createMatrix((maxY + 2) * 2 + 1, maxY + 1, '.') as string[][];
+  canvas.push(
+    ...[
+      new Array<string>(canvas[0].length).fill('.'),
+      new Array<string>(canvas[0].length).fill('#'),
+    ]
+  );
+
+  const xOffset = 500 - Math.floor(canvas[0].length / 2);
+
+  canvas[0][500 - xOffset] = '+';
+
+  input.forEach(p => {
+    const [start, ...dests] = p.split(' -> ').map(
+      pos =>
+        ({
+          x: Number(pos.split(',')[0]) - xOffset,
           y: Number(pos.split(',')[1]),
         }) as Position
     );
