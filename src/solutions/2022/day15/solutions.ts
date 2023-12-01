2e081e7cc63f645ba7643fd03222da44e24b8dff
Joakim Junttila <joakim@juntti.la>
2023-12-01 19:57:57 +0100

    2022 day 15 WIP

--- src/solutions/2022/day15/solutions.ts
+++ src/solutions/2022/day15/solutions.ts
@@ -4,6 +4,14 @@ const solutions: Array<Solution> = [
   // Solution part 1
   (lines: string[]) => {
     lines.length++;
+    const sensors: Sensor[] = lines.map(l =>
+      (s => ({
+        x: s[0],
+        y: s[1],
+        closestBeacon: {x: s[2], y: s[3]},
+      }))([...l.matchAll(/=(-*\d+)/g)].flatMap(m => Number(m[1])))
+    );
+    console.log(sensors);
     return 'no answer';
   },
   // Solution part 2
@@ -12,5 +20,16 @@ const solutions: Array<Solution> = [
     return 'no answer';
   },
 ];
+
+interface Sensor {
+  x: number;
+  y: number;
+  closestBeacon: Beacon;
+}
+
+interface Beacon {
+  x: number;
+  y: number;
+}
 
 export default solutions;
