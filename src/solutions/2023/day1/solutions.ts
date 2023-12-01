bcafb73fdb4e37b5194a9bd648189885f9cb0c63
Joakim Junttila <joakim@juntti.la>
2023-12-01 18:31:39 +0100

    Cursed Apdo challenge

--- src/solutions/2023/day1/solutions.ts
+++ src/solutions/2023/day1/solutions.ts
@@ -11,33 +11,32 @@ const solutions: Array<Solution> = [
       .reduce((acc, l) => acc + Number(l) || 0, 0);
   },
   // Solution part 2
-  (lines: string[]) => {
-    const numberRegex = new RegExp(
-      `(?=(\\d|${Object.keys(numbers).join('|')}))`,
-      'g'
-    );
-    return lines
-      .map(l => {
-        const split = Array.from(l.matchAll(numberRegex))
-          .flat()
-          .filter(m => m)
-          .map(m => Number(m) || numbers[m]);
-        return `${split.at(0)}${split.at(-1)}`;
-      })
-      .reduce((acc, l) => acc + Number(l), 0);
-  },
+  (lines: string[]) =>
+    (numbers =>
+      lines
+        .map(l =>
+          (s => `${s.at(0)}${s.at(-1)}`)(
+            Array.from(
+              l.matchAll(
+                new RegExp(`(?=(\\d|${Object.keys(numbers).join('|')}))`, 'g')
+              )
+            )
+              .flat()
+              .filter(m => m)
+              .map(m => Number(m) || numbers[m])
+          )
+        )
+        .reduce((acc, l) => acc + Number(l), 0))({
+      one: 1,
+      two: 2,
+      three: 3,
+      four: 4,
+      five: 5,
+      six: 6,
+      seven: 7,
+      eight: 8,
+      nine: 9,
+    } as {[_: string]: number}),
 ];
-
-const numbers: {[_: string]: number} = {
-  one: 1,
-  two: 2,
-  three: 3,
-  four: 4,
-  five: 5,
-  six: 6,
-  seven: 7,
-  eight: 8,
-  nine: 9,
-};
 
 export default solutions;
