0ba732facb19a65b028068cb3cd1edc8aa161bd2
Joakim Junttila <joakim@juntti.la>
2023-12-04 17:21:28 +0100

    Fix createMatrix

--- src/solutions/scripts/createMatrix.ts
+++ src/solutions/scripts/createMatrix.ts
@@ -1,8 +1,4 @@
-export function createMatrix<T>(
-  width: number,
-  height: number,
-  fill: T | null = null
-) {
+export function createMatrix<T>(width: number, height: number, fill: T) {
   return Array.from({length: height}, () =>
     Array.from({length: width}, () => fill)
   );
