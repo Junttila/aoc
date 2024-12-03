775b4e177ecee8a77afe8ec437efa06d2aca6390
Joakim Junttila <joakim@juntti.la>
2024-12-03 16:56:52 +0100

    2024 day 3 solved (typescript)

--- typescript/src/solutions/2024/day3/solutions.ts
+++ typescript/src/solutions/2024/day3/solutions.ts
@@ -3,13 +3,28 @@ import {Solution} from '../../../types';
 const solutions: Array<Solution> = [
   // Solution part 1
   (lines: string[]) => {
-    lines.length++;
-    return 'no answer';
+    const matches = [...lines.join().matchAll(/mul\((\d+),(\d+)\)/g)].map(m =>
+      m.slice(1).map(Number)
+    );
+
+    const sum = matches.reduce((acc, m) => acc + m[0] * m[1], 0);
+    return sum;
   },
   // Solution part 2
   (lines: string[]) => {
-    lines.length++;
-    return 'no answer';
+    const doStrings = lines
+      .join()
+      .split('do()')
+      .map(s => s.split("don't()")[0]);
+
+    const matches = doStrings.flatMap(l =>
+      [...l.matchAll(/mul\((\d+),(\d+)\)/g)].map(m => {
+        return m.slice(1).map(Number);
+      })
+    );
+
+    const sum = matches.reduce((acc, m) => acc + m[0] * m[1], 0);
+    return sum;
   },
 ];
 
