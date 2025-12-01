9060578a46b6042d40e239fc10d61a75451dbf49
Joakim Junttila <joakim.junttila@standbygroup.com>
2025-12-01 12:12:24 +0100

    Init year 2025

---
+++ typescript/src/solutions/2025/day17/solutions.ts
@@ -0,0 +1,16 @@
+import {Solution} from '../../../types';
+
+const solutions: Array<Solution> = [
+  // Solution part 1
+  (lines: string[]) => {
+    lines.length++;
+    return 'no answer';
+  },
+  // Solution part 2
+  (lines: string[]) => {
+    lines.length++;
+    return 'no answer';
+  },
+];
+
+export default solutions;
