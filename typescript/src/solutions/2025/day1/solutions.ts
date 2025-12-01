ea2f23959dd0f7549506b61db77ad858ba3987ae
Joakim Junttila <joakim.junttila@standbygroup.com>
2025-12-01 13:08:55 +0100

    2025 day 1 part 1 solved

--- typescript/src/solutions/2025/day1/solutions.ts
+++ typescript/src/solutions/2025/day1/solutions.ts
@@ -4,7 +4,19 @@ const solutions: Array<Solution> = [
   // Solution part 1
   (lines: string[]) => {
     lines.length++;
-    return 'no answer';
+    const result = lines.reduce(
+      (acc, l) => {
+        const dir = l.substring(0, 1);
+        const distance = Number(l.substring(1));
+        if (Number.isNaN(distance)) return acc;
+        acc.position += dir === 'R' ? distance : dir === 'L' ? -distance : 0;
+        acc.position %= 100;
+        acc.zeroes += acc.position === 0 ? 1 : 0;
+        return acc;
+      },
+      {position: 50, zeroes: 0}
+    );
+    return result.zeroes;
   },
   // Solution part 2
   (lines: string[]) => {
