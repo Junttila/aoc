32d9f1f15cad67f05b5e8e6a2388030a86fab638
Joakim Junttila <joakim.junttila@standbygroup.com>
2025-12-02 12:52:06 +0100

    2025 day 1 part 2 solved

--- typescript/src/solutions/2025/day1/solutions.ts
+++ typescript/src/solutions/2025/day1/solutions.ts
@@ -3,7 +3,6 @@ import {Solution} from '../../../types';
 const solutions: Array<Solution> = [
   // Solution part 1
   (lines: string[]) => {
-    lines.length++;
     const result = lines.reduce(
       (acc, l) => {
         const dir = l.substring(0, 1);
@@ -20,8 +19,27 @@ const solutions: Array<Solution> = [
   },
   // Solution part 2
   (lines: string[]) => {
-    lines.length++;
-    return 'no answer';
+    const result = lines.reduce(
+      (acc, l) => {
+        const dir = l.substring(0, 1);
+        const distance = Number(l.substring(1));
+        if (Number.isNaN(distance)) return acc;
+        const movement = dir === 'R' ? distance : dir === 'L' ? -distance : 0;
+        const prevPos = acc.position;
+        acc.position += movement;
+        acc.zeroes +=
+          acc.position > 0
+            ? Math.floor(acc.position / 100)
+            : acc.position < 0
+            ? (prevPos !== 0 ? 1 : 0) + Math.floor(Math.abs(acc.position) / 100)
+            : 1;
+        acc.position %= 100;
+        acc.position += acc.position < 0 ? 100 : 0;
+        return acc;
+      },
+      {position: 50, zeroes: 0}
+    );
+    return result.zeroes;
   },
 ];
 
